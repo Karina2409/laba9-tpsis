538b81cc1654b9cdee7d90bdb56b845ff50a5939
Karina2409 <karina.siardziuk@mail.ru>
2024-04-07 14:56:24 +0300

    pages2

--- register-page.js
+++ register-page.js
@@ -3,6 +3,14 @@ function registration(){
   const login = document.querySelector('.js-input-login').value;
   const password = document.querySelector('.js-input-password').value;
 
+  const name1 = document.querySelector('.js-input-name').value;
+  const login1 = document.querySelector('.js-input-login').value;
+  const password1 = document.querySelector('.js-input-password').value;
+
+  const name2 = document.querySelector('.js-input-name').value;
+  const login2 = document.querySelector('.js-input-login').value;
+  const password2 = document.querySelector('.js-input-password').value;
+
   if(name !== "" && login !== "" && password !== ""){
     window.location.href='profile-page.html';
 
