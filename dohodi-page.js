d794352df4c7a0ab682c9ac6f51675f07b144e3a
Karina2409 <karina.siardziuk@mail.ru>
2024-04-07 15:04:18 +0300

    pages5

---
+++ dohodi-page.js
@@ -0,0 +1,24 @@
+function back(){
+  window.location.href='main-page.html';
+}
+
+function analis(){
+  window.location.href='analis-page.html';
+}
+
+function otcheti(){
+  window.location.href='otcheti-page.html';
+}
+
+function aboutUs(){
+  window.location.href='about-us-page.html';
+}
+
+function data(){
+  window.location.href='data-page.html';
+}
+
+function profile(){
+  window.location.href='profile-page.html';
+}
+
