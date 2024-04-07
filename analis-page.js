52b9b8108c918d2b4a6799b4d51452fa6322f3c2
Karina2409 <karina.siardziuk@mail.ru>
2024-04-07 15:02:16 +0300

    pages5

---
+++ analis-page.js
@@ -0,0 +1,23 @@
+function back(){
+  window.location.href='main-page.html';
+}
+
+function dohodi(){
+  window.location.href='dihodi-page.html';
+}
+
+function otcheti(){
+  window.location.href='otcheti-page.html';
+}
+
+function aboutUs(){
+  window.location.href='about-us-page.html';
+}
+
+function data(){
+  window.location.href='data-page.html';
+}
+
+function profile(){
+  window.location.href='profile-page.html';
+}
---
+++ otcheti-page.js
@@ -0,0 +1,23 @@
+function back(){
+  window.location.href='main-page.html';
+}
+
+function dohodi(){
+  window.location.href='dihodi-page.html';
+}
+
+function analis(){
+  window.location.href='analis-page.html';
+}
+
+function aboutUs(){
+  window.location.href='about-us-page.html';
+}
+
+function data(){
+  window.location.href='data-page.html';
+}
+
+function profile(){
+  window.location.href='profile-page.html';
+}
